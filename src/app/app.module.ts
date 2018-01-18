58da2d5ddcc87655dc1ae4541e6e2a010218c9a6
PrafullJoshi <prafull.joshi@gmail.com>
2018-01-18 07:51:40 +0530

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthParticipantPageModule } from '../pages/auth-participant/auth-participant.module';
+import { SponsorerPageModule } from '../pages/sponsorer/sponsorer.module';
+import { NotaryPageModule } from '../pages/notary/notary.module';
+import { AuthParticipantPage } from '../pages/auth-participant/auth-participant';
+import { SponsorerPage } from '../pages/sponsorer/sponsorer';
+import { NotaryPage } from '../pages/notary/notary';
+import { SharedServie } from '../shared/shared.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +29,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    AuthParticipantPageModule,
+    SponsorerPageModule,
+    NotaryPageModule,
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,11 +41,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    AuthParticipantPage,
+    SponsorerPage,
+    NotaryPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    SharedServie,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { NotaryPage } from '../notary/notary';
+import { SponsorerPage } from '../sponsorer/sponsorer';
+import { AuthParticipantPage } from '../auth-participant/auth-participant';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +13,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabAp = AuthParticipantPage;
+  tabSponsorer = SponsorerPage;
+  tabNotary = NotaryPage;
 
   constructor() {
 
